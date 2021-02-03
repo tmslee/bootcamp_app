6b57e0d48a0904a4cd2b4f18ba7e910ea646b276
Sang Hyup Lee <tms.lee9@gmail.com>
2021-02-03 22:52:34 +0000

    implement parametrized query

--- teachers.js
+++ teachers.js
@@ -8,16 +8,11 @@ const pool = new Pool({
 });
 
 const args = process.argv.splice(2);
+const values = [`${args[0]}`];
 
-pool.query(`
-SELECT DISTINCT t.name, c.name AS cohort
-FROM assistance_requests ar
-  JOIN teachers t ON t.id = ar.teacher_id
-  JOIN students s ON s.id = ar.student_id
-  JOIN cohorts c ON c.id = s.cohort_id
-WHERE c.name = '${args[0]}'
-ORDER BY t.name;
-`)
+pool.query(
+"SELECT DISTINCT t.name, c.name AS cohort FROM assistance_requests ar JOIN teachers t ON t.id = ar.teacher_id JOIN students s ON s.id = ar.student_id JOIN cohorts c ON c.id = s.cohort_id WHERE c.name = $1 ORDER BY t.name;"
+, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
